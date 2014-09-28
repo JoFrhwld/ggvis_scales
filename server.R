96b8ad68e0809ee6aabf2ff640d2c2c025fe4c0e
Josef Fruehwald <JoFrhwld@gmail.com>
2014-09-28 13:42:36 +0100

    this was my original approach

--- server.R
+++ server.R
@@ -25,15 +25,11 @@ shinyServer(function(input, output) {
     
     output$jawn <- renderText(colorRange())
     
-    vis <- reactive({
-      cols <- colorRange()
-      hilight_data <- selectedData()
       
-      mtcars%>%
-        ggvis(~wt, ~mpg)%>%
-        layer_points()%>%
-        layer_points(data = hilight_data, fill = ~gear)%>%
-        scale_ordinal("fill", range = cols)      
-    })
-    vis %>% bind_shiny("ggvis", "ggvis_ui")
+    mtcars%>%
+      ggvis(~wt, ~mpg)%>%
+      layer_points()%>%
+      layer_points(data = selectedData, fill = ~gear)%>%
+      scale_ordinal("fill", range = colorRange) %>% 
+      bind_shiny("ggvis", "ggvis_ui")
 })
